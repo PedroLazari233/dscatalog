a3baadc6e6435c868f71cc0677e3e0f421ae4269
Pedro Lazari <67986647+PedroLazari233@users.noreply.github.com>
2022-08-15 21:59:28 -0300

    Auditing data

--- backend/src/main/java/com/pedrolazari/dscatalog/entities/Category.java
+++ backend/src/main/java/com/pedrolazari/dscatalog/entities/Category.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.Instant;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,4 +20,19 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant UpdateAt;
+
+    @PrePersist
+    public void prePersist(){
+        createdAt = Instant.now();
+    }
+    @PreUpdate
+    public void preUpdate(){
+        UpdateAt = Instant.now();
+    }
 }
